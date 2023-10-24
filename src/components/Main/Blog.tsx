9fad6d3d5e9f84101713608e20e120c3d217247e
Evgeny_Grinchuk <n0name-xd@inbox.ru>
2023-10-24 21:03:07 +0300

    feat: add elipses

--- src/components/Main/Blog.tsx
+++ src/components/Main/Blog.tsx
@@ -3,8 +3,42 @@ import Image from "next/image";
 export const Blog = (): JSX.Element => {
   return (
     <div className="main-blog">
-      <div></div>
-      <div></div>
+      <div>
+        <h3>
+          Summer cactus <br /> & succulents
+        </h3>
+        <p>
+          We arean online plant shop offering a wide <br /> range of cheap and
+          trendy plants
+        </p>
+        <button className="custom-button">
+          Find more
+          <Image
+            alt="arrow left"
+            src="/icons/arrow-right2.svg"
+            width={15}
+            height={15}
+          />
+        </button>
+      </div>
+      <div>
+        <h3>
+          Styling trends <br /> & much more
+        </h3>
+        <p>
+          We arean online plant shop offering a wide <br /> range of cheap and
+          trendy plants
+        </p>
+        <button className="custom-button">
+          Find more
+          <Image
+            alt="arrow left"
+            src="/icons/arrow-right2.svg"
+            width={15}
+            height={15}
+          />
+        </button>
+      </div>
     </div>
   );
 };
