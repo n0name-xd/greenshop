0db245a7ae965817009664bc9d2f17bbfec3b068
Evgeny_Grinchuk <n0name-xd@inbox.ru>
2023-10-25 21:26:30 +0300

    feat: add top footer

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Green Shop
 
+start: 19.10.2023
+
 Created according to the layout:
 
 https://www.figma.com/file/T9fNIlcua3pIuuZuBoQPOQ/%D1%80%D0%B0%D1%81%D1%82%D0%B5%D0%BD%D0%B8%D1%8F-(Copy)?node-id=2%3A90&mode=dev
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,46 @@
+import Image from "next/image";
+
 export const Footer = () => {
   return (
     <footer>
-      <div className="footer__top">1</div>
+      <div className="footer__top">
+        <div className="footer__plants plants-one">
+          <Image alt="plant" src="/footer/bush.svg" width={61} height={46} />
+          <h4>Garden Care</h4>
+          <p className="footer__text">
+            We are an online plant shop offering a wide range of cheap and
+            trendy plants.
+          </p>
+        </div>
+        <div className="footer__plants plants-two">
+          <Image alt="plant" src="/footer/bush2.svg" width={61} height={46} />
+          <h4>Plant Renovation</h4>
+          <p className="footer__text">
+            We are an online plant shop offering a wide range of cheap and
+            trendy plants.
+          </p>
+        </div>
+        <div className="footer__plants plants-three">
+          <Image alt="plant" src="/footer/bush3.svg" width={61} height={46} />
+          <h4>Watering Graden</h4>
+          <p className="footer__text">
+            We are an online plant shop offering a wide range of cheap and
+            trendy plants.
+          </p>
+        </div>
+        <div className="newsletters">
+          <h4>Would you like to join newsletters?</h4>
+          <div className="newsletters__input">
+            <input placeholder="enter your email address..." type="text" />
+            <button>Join</button>
+          </div>
+          <p className="footer__text">
+            We usually post offers and challenges in newsletter. We’re your
+            online houseplant destination. We offer a wide range of houseplants
+            and accessories shipped directly from our (green)house to yours!
+          </p>
+        </div>
+      </div>
       <div className="footer__middle">2</div>
       <div className="footer__low">3</div>
       <div className="coppyright">© 2021 GreenShop. All Rights Reserved.</div>
